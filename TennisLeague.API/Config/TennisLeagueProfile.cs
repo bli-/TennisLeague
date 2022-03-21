362387404b9ae0c25cacfc9a87ba6e4b3f0ed85e
Bill Huynh <bhuynh@ethosgroup.com>
2022-03-21 15:13:11 -0500

    Flesh out facility and add automapper

---
+++ TennisLeague.API/Config/TennisLeagueProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TennisLeague.Data;
+
+namespace TennisLeague.API.Config
+{
+    public class TennisLeagueProfile : Profile
+    {
+        public TennisLeagueProfile()
+        {
+            CreateMap<Facility, Models.Facility>();
+            CreateMap<Models.Facility, Facility>();
+
+            CreateMap<Player, Models.Player>();
+            CreateMap<Models.Player, Player>();
+        }
+    }
+}
